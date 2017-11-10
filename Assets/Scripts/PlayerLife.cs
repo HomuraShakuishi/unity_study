65125bcfaf581100cf5f9bbc17a03201140ef411
AyumiYasui <anzu.ya0627@gmail.com>
2017-11-10 23:02:30 +0900

    add UnityProject and .gitignore

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour {
+
+	public readonly int maxLife = 100; //プレイヤーの体力最大値、あとでここを変えられるようにする
+	public int Life;
+	public int EnemyATK = 10;//敵の攻撃力　あとでここも(ry
+
+
+	// Use this for initialization
+	void Start () {
+		Life = maxLife;//初期状態は体力が最大値
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Life <= 0) {
+			Debug.Log ("GameOver");
+		}
+	}
+
+	void OntriggerEnter (BoxCollider2D hit){
+
+		if (hit.CompareTag ("Bullet")) {
+			Life -= EnemyATK;
+			Debug.Log ("当たったよ");
+			//Debug.Log (Life);
+
+		}
+	}
+}
